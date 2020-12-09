55305588332f4e2164aa837d091bf32a5b6ee78f
Tomer Bitan <tbitan@gmail.com>
2020-12-09 21:59:19 +0200

    fixed ds error

--- library.cpp
+++ library.cpp
@@ -10,7 +10,7 @@ StatusType AddCourse (void *DS, int courseID, int numOfClasses);
 {
     if(DS == NULL)
     {
-        return FAILURE;
+        return INVALID_INPUT;
     }
     return DS->AddCourse(courseID,numOfClasses);
 }
@@ -19,7 +19,7 @@ StatusType RemoveCourse(void *DS, int courseID)
 {
     if(DS == NULL)
     {
-        return FAILURE;
+        return INVALID_INPUT;
     }
     return DS->RemoveCourse(courseID);
 }
@@ -28,7 +28,7 @@ StatusType WatchClass(void *DS, int courseID, int classID, int time)
 {
     if(DS == NULL)
     {
-        return FAILURE;
+        return INVALID_INPUT;
     }
     return DS->WatchClass(courseID,classID,time);
 }
@@ -37,7 +37,7 @@ StatusType TimeViewed(void *DS, int courseID, int classID, int *timeViewed)
 {
     if(DS == NULL)
     {
-        return FAILURE;
+        return INVALID_INPUT;
     }
     return DS->WatchClass(courseID,classID,timeViewed);
 }
@@ -46,7 +46,7 @@ StatusType GetMostViewedClasses(void *DS, int numOfClasses, int *courses, int *c
 {
     if(DS == NULL)
     {
-        return FAILURE;
+        return INVALID_INPUT;
     }
     return DS->WatchClass(numOfClasses,courses,classes);
 }
